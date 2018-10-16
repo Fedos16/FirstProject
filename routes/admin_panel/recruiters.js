38e82cd9ad5ffadb048999f90d7e14969966d4df
olegator072@gmail.com <Asser220_>
2018-10-16 22:32:47 +0300

    test designe 1.0.0

--- routes/admin_panel/recruiters.js
+++ routes/admin_panel/recruiters.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const models = require('../../models');
 
-router.get('/edit', (req, res) => {
+router.get('/edit', async (req, res) => {
 
       
     const login = req.session.userLogin;
@@ -14,53 +14,27 @@ router.get('/edit', (req, res) => {
     }
     if (login){
 
-        models.Recruiter.find((err, recruiter) => {
+        var data = {
+            user: login,
+            typeUser: userType,
+            page: page
+        }
 
-            const data = {
-                user: login,
-                typeUser: userType,
-                page: page,
-                fio: [],
-                location: [],
-                telephone: [],
-                directions: [],
-                directions_short: [],
-                id: [],
-                status: []
-            }
+        try {
+            var rec = await models.Recruiter.find({}, {fio: 1, residence: 1, telephone: 1, directions: 1, status: 1});
+            data.rec = rec;
+        } catch (e) {
+            data.rec = [];
+        }
 
-            if (err) res.render('./recruiters/edit', {data});
-            
-            for (var i=0; i < recruiter.length; i++){
-                
-                data.fio.push(recruiter[i].fio);
-                data.location.push(recruiter[i].location);
-                data.telephone.push(recruiter[i].telephone);
-                data.directions.push(recruiter[i].directions);
-                data.id.push(recruiter[i]._id);
-                data.status.push(recruiter[i].status);
-
-                var arr_directions = recruiter[i].directions.split(', ');
-                if (arr_directions.length > 3){
-                    arr_directions = arr_directions.slice(0, 3);
-                    data.directions_short.push(arr_directions.join(', ') + ' ...');
-                    arr_directions = [];
-                } else {
-                    data.directions_short.push(recruiter[i].directions);
-                }
-
-                
-            }
-
-            res.render('./recruiters/edit', {data});
+        res.render('./recruiters/edit', {data});
         
-        });
     }
     else{
         res.redirect('/administration/');
     }
 });
-router.get('/moderation', (req, res) => {
+router.get('/moderation', async (req, res) => {
     const login = req.session.userLogin;
     const userType = req.session.userType;
     const page = {
@@ -74,13 +48,14 @@ router.get('/moderation', (req, res) => {
     }
     if (login){
 
-        models.Recruiter.find({status: 'Модерация'}, {fio: 1, residence: 1, telephone: 1, directions: 1}, (errs, recruiter) => {
-            if (errs) res.render('./recruiters/moderation', {data});
+        try{
+            var rec = await models.Recruiter.find({status: 'Модерация'}, {fio: 1, residence: 1, telephone: 1, directions: 1});
+            data.rec = rec;
+        } catch(e){
+            data.rec = [];
+        }
 
-            data.recruiter = recruiter;
-
-            res.render('./recruiters/moderation', {data});
-        });
+        res.render('./recruiters/moderation', {data});
 
     }
     else{
@@ -98,19 +73,19 @@ router.get('/new', (req, res) => {
     }
 
 });
-router.get('/r:id', (req, res) => {
+router.get('/id:id', async (req, res) => {
 
     const id = req.params.id;
 
-    models.Recruiter.findOne( {_id: id, status: 'Редактирование'}, (errs, recruiter) => {
-        if (errs) {
-            res.send('ERROR!');
-        }else{
-            var fio = recruiter.fio;
-            console.log(fio);
-            res.render('./recruiters/editing_user', {fio});
-        }
-    });
+    var rec
+    try {
+        rec = await models.Recruiter.findOne( {_id: id, status: 'Редактирование'});
+    } catch (e) {
+        rec = {};
+    }
+
+    res.render('./recruiters/editing_user', {rec});
+
 });
 router.post('/findrecruiter', (req, res) => {
     
